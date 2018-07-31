19bdaa376b838ea5e1d546456847f6dd2c9fa888
Squareroot <squareroot@Squareroots-iMac-4.local>
2018-07-30 22:20:31 -0700

    - Create basic structure - Completed splash screen activity. - Add Icons and add launcher icon

--- app/src/main/java/com/deuro/android/Activitys/Splash_Activity.java
+++ app/src/main/java/com/deuro/android/Activitys/Splash_Activity.java
@@ -2,11 +2,11 @@ package com.deuro.android.Activitys;
 
 import android.content.Intent;
 import android.os.Build;
+import android.os.Bundle;
 import android.os.Handler;
 import android.support.annotation.RequiresApi;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
 
